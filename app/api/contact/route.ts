042ef3b25dc604cbf107ec181c7df539eba4388e
Anshika Tiwari <tiwarianshikatiwari6@gmail.com>
2026-04-29 15:32:50 +0530

    email_smtp_added

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -56,7 +56,9 @@ export async function POST(req: Request) {
 
     //  Send Email
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
+      secure: true, // true for 465, false for 587
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
